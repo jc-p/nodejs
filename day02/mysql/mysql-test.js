daf1a72864eddf88b92c2cb9a98e412c25e506b0
Anpjc <32951547+Anpjc@users.noreply.github.com>
2017-11-14 09:48:11 +0800

    node学习

--- day02/mysql/mysql-test.js
+++ day02/mysql/mysql-test.js
@@ -1,5 +1,16 @@
 /**
  * Created by web-01 on 2017/11/13.
  */
-const  mysql=require("mysql");
-console.log(mysql);
+const  mysql=require('mysql');
+// console.log(mysql);
+let connection = mysql.createConnection({user:'root'});
+
+connection.connect((err)=>{
+    if(err) throw err;
+    let sql='INSERT INTO web1706a.stu VALUES(NULL,?,?,?)';
+    connection.query(sql,['1','2','3'],(err,results,fields)=>{
+        if(err) throw err;
+        console.log(results.affectedRows);
+        console.log(results.insertId);
+    })
+});
