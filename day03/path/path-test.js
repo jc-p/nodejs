449430b59dd8aa9f9dd0b6d029c4e6ed3c4c55b6
Anpjc <32951547+Anpjc@users.noreply.github.com>
2017-11-14 10:24:13 +0800

    node学习

--- day03/path/path-test.js
+++ day03/path/path-test.js
@@ -5,3 +5,7 @@ const path=require('path');
 
 console.log(path.normalize('/test1//test2'));
 console.log(path.join(__dirname,'../test/test2','test3/test4'));
+
+
+console.log(path.isAbsolute(__filename));
+console.log(path,extname(__filename));//extension name 文件扩展名
